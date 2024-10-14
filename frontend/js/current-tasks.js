95b749a06a106a09bdcb3690a830f743196f09ac
Brytalisk <roman.slipak@gmail.com>
2024-10-14 21:09:37 +0300

    Delete code from current-tasks

--- frontend/js/current-tasks.js
+++
@@ -1,8 +0,0 @@
-const boxForCheckedChange = document.querySelector(".current-tasks__box");
-
-boxForCheckedChange.addEventListener("change", (event) => {
-    if (event.target.classList.contains("task-checkbox")) {
-        const taskCard = event.target.closest(".task-card");
-        taskCard.classList.toggle("task-card--completed");
-    }
-});
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,5 @@
 document.addEventListener("partialsLoaded", () => {
     import("./modal-window.js");
     import("./main.js");
-    import("./current-tasks.js");
 });
 
